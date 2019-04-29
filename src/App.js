7e3822c4d2c2472ca8d6dad8a606e5eb5fa32468
kmocorro <kevinmocorro.github@gmail.com>
2019-04-29 15:18:36 +0800

    aweaweoijawe

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
-
+import Auther from './components/Auther';
+// hehe
 export default () => {
+
+    function isLoggedIn(){
+        let tokener = getToken();
+        return tokener; //blnk false
+    }
+
+    function getToken(){
+        return localStorage.getItem('ldap_token');
+    }
 
     return (
         <div>
-            <h1>hello</h1>
+            { isLoggedIn()
+                ? <h1>hello</h1>
+                : <Auther />
+            }
         </div>
 
     )
